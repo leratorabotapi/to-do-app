fe2f98a8519b3da4ce6bedbf1474b36830261c1a
lerato <leratorabotapi@hotmail.com>
2019-11-15 15:21:34 +0200

    Updates

---
+++ precache-manifest.9b6ca0591309bb3d582c4abad5b51916.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60e6816a297c28fee7c8",
+    "url": "/to-do-app/static/js/runtime~main.60e6816a.js"
+  },
+  {
+    "revision": "289bc832cbedc74ec171",
+    "url": "/to-do-app/static/js/main.289bc832.chunk.js"
+  },
+  {
+    "revision": "c5d2667536a462e5b8b4",
+    "url": "/to-do-app/static/js/1.c5d26675.chunk.js"
+  },
+  {
+    "revision": "289bc832cbedc74ec171",
+    "url": "/to-do-app/static/css/main.3e4bef05.chunk.css"
+  },
+  {
+    "revision": "f8c3fc4ce9a985abadb567bdb9518ea7",
+    "url": "/to-do-app/index.html"
+  }
+];
